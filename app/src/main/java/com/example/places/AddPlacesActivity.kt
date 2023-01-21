fbcb2930f0126a117fa213d105c2c52c4a5ab8ba
m0nadicph0 <m0nadic.ph0@gmail.com>
2023-01-21 16:42:11 +0530

    add back button on add places activity

--- app/src/main/java/com/example/places/AddPlacesActivity.kt
+++ app/src/main/java/com/example/places/AddPlacesActivity.kt
@@ -2,10 +2,17 @@ package com.example.places
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class AddPlacesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_places)
+        val toolbarAddPlaces = findViewById<Toolbar>(R.id.toolbar_add_places)
+        setSupportActionBar(toolbarAddPlaces)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbarAddPlaces.setNavigationOnClickListener{
+            onBackPressed()
+        }
     }
 }
