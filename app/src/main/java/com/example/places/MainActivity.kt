22c94a8b39baf5c87ff3ab253b5146e36c35ad0a
m0nadicph0 <m0nadic.ph0@gmail.com>
2023-01-21 16:34:41 +0530

    basic styles and new activity

--- app/src/main/java/com/example/places/MainActivity.kt
+++ app/src/main/java/com/example/places/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.places
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<FloatingActionButton>(R.id.fab_add_places).setOnClickListener{
+            val intent = Intent(this, AddPlacesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
